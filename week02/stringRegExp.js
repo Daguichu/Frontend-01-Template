bcd0d82fc4d2a43fa47b8b1a27104b72e96b8df5
chuchen <18725716749@163.com>
2020-04-23 10:10:11 +0800

    fix: stringRegExp

--- week02/stringRegExp.js
+++ week02/stringRegExp.js
@@ -1,1 +1,9 @@
-const string = /.*/;
+//转义字符
+const escape = /(\\['"\\bfnrtv])/;
+//空格符
+const espace = /[(\\u000A)(\\u000D)(\\u2028)(\\u2029)]/;
+//十六进制
+const hex = /\\x[0-9a-fA-F]{2}/;
+const unicode = /\\u[0-9a-fA-F]{4}/;
+
+const string = /^"[^"\\]*[(\\u000A)(\\u000D)(\\u2028)(\\u2029)]*(\\['"\\bfnrtv])*(\\x[0-9a-fA-F]{2})*(\\u[0-9a-fA-F]{4})*"$/;
