b23e9faca9a3b5f05c878002bef0df4e283307f0
chuchen <18725716749@163.com>
2020-04-30 15:26:33 +0800

    update: convertNumberToString

--- week03/Thursday/convertNumberToString.js
+++ week03/Thursday/convertNumberToString.js
@@ -1,25 +1,31 @@
-function convertNumberToString(number, x = 10) {
+function convertNumberToString(number) {
   var integer = Math.floor(number);
   var fraction = number - integer;
-  var string = "";
-  if (x === 2 || x === 8 || x === 16) {
-    return integer + "";
-  }
+  var string = integer === 0 ? "0" : "";
+  var strFraction = ".";
   while (integer > 0) {
-    string = String(integer % x) + string;
-    integer = Math.floor(integer / x);
+    string = String(integer % 10) + string;
+    integer = Math.floor(integer / 10);
+  }
+  while (fraction > 0) {
+    fraction = fraction * 10;
+    let int = Math.floor(fraction);
+    strFraction = strFraction + int;
+    fraction = fraction - int;
   }
 
-  return number + "";
+  return strFraction === "." ? string : string + strFraction;
 }
 
-console.log(convertNumberToString(0b0000, 2));
-console.log(convertNumberToString(0b0111, 2));
-console.log(convertNumberToString(0o71, 8));
-console.log(convertNumberToString(0o46, 8));
-console.log(convertNumberToString(0xa, 2));
-console.log(convertNumberToString(0x1f, 2));
+console.log(convertNumberToString(0b0000));
+console.log(convertNumberToString(0b0111));
+console.log(convertNumberToString(0o71));
+console.log(convertNumberToString(0o46));
+console.log(convertNumberToString(0xa));
+console.log(convertNumberToString(0x1f));
 console.log(convertNumberToString(0));
-console.log(convertNumberToString(1234, 10));
-console.log(convertNumberToString(1234.123, 10));
-console.log(convertNumberToString(0.1234, 10));
+console.log(convertNumberToString(1234));
+console.log(convertNumberToString(1234.3));
+console.log(convertNumberToString(0.5));
+console.log(convertNumberToString(1E3));
+
