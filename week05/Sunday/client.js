b81d14b72b485e672ba05b87b6ae2e2be42e76cb
chuchen <18725716749@163.com>
2020-05-15 10:19:21 +0800

    ups

--- week05/Sunday/client.js
+++ week05/Sunday/client.js
@@ -5,7 +5,7 @@ class Request {
     this.method = options.method || "GET";
     this.host = options.host;
     this.port = options.port || 80;
-    this.path = options.path || '/';
+    this.path = options.path || "/";
     this.headers = options.headers || {};
     this.body = options.body || {};
 
@@ -36,44 +36,172 @@ ${this.bodyText}
 `;
   }
 
-  send(connection){
+  send(connection) {
     return new Promise((resolve, reject) => {
-      if(connection){
+      const parse = new ResponseParse();
+      if (connection) {
         connection.write(this.toString());
-      }else {
-        connection = net.createConnection({ host: this.host, port: this.port }, () => {
-          connection.write(this.toString());
-        })
+      } else {
+        connection = net.createConnection(
+          { host: this.host, port: this.port },
+          () => {
+            connection.write(this.toString());
+          }
+        );
       }
-      connection.on('data', (data) => {
-        resolve(data.toString());
+      connection.on("data", (data) => {
+        parse.receive(data.toString());
+        console.log(parse.statusLine);
+        console.log(parse.headers);
+        //resolve(data.toString());
         connection.end();
-      })
-      connection.on('error', (err) => {
+      });
+      connection.on("error", (err) => {
         reject(err);
         connection.end();
-      })
-    }) 
+      });
+    });
   }
 }
 
-void async function(){
+class ResponseParse {
+  constructor() {
+    this.WAITING_STATUS_LINE = 0;
+    this.WAITING_STATUS_LINE_END = 1;
+    this.WAITING_HEADER_NAME = 2;
+    this.WAITING_HEADER_SPACE = 3;
+    this.WAITING_HEADER_VALUE = 4;
+    this.WAITING_HEADER_LINE_END = 5;
+    this.WAITING_STATUS_BLOCK_END = 6;
+    this.WAITING_BODY = 7;
+
+    this.current = this.WAITING_STATUS_LINE;
+    this.statusLine = "";
+    this.headers = {};
+    this.headerName = "";
+    this.headerValue = "";
+    this.bodyParse = null;
+  }
+
+  receive(string) {
+    for (let i = 0, len = string.length; i < len; i++) {
+      this.receiveChart(string.charAt(i));
+    }
+  }
+
+  receiveChart(chart) {
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (chart === "\r") {
+        this.current = this.WAITING_STATUS_LINE_END;
+      } else {
+        this.statusLine += chart;
+      }
+    } else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (chart === "\n") {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITING_HEADER_NAME) {
+      if (chart === ":") {
+        this.current = this.WAITING_HEADER_SPACE;
+      } else if (chart === "\r") {
+        this.current = this.WAITING_STATUS_BLOCK_END;
+        if (this.headers["Transfer-Encoding"] === "chunked") {
+          this.bodyParse = new TrunkeBodyParse();
+        }
+      } else {
+        this.headerName += chart;
+      }
+    } else if (this.current === this.WAITING_HEADER_SPACE) {
+      if (chart === " ") {
+        this.current = this.WAITING_HEADER_VALUE;
+      }
+    } else if (this.current === this.WAITING_HEADER_VALUE) {
+      if (chart === "\r") {
+        this.current = this.WAITING_HEADER_LINE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerName = "";
+        this.headerValue = "";
+      } else {
+        this.headerValue += chart;
+      }
+    } else if (this.current === this.WAITING_HEADER_LINE_END) {
+      if (chart === "\n") {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITING_STATUS_BLOCK_END) {
+      if (chart === "\n") {
+        this.current = this.WAITING_BODY;
+      }
+    } else if (this.current === this.WAITING_BODY) {
+      this.bodyParse.receiveChar(chart);
+    }
+  }
+}
+
+class TrunkeBodyParse {
+  constructor() {
+    this.WAITING_LENGTH = 0;
+    this.WAITING_LENGTH_LINE_END = 1;
+    this.READING_TRUNK = 2;
+    this.WAITING_NEW_LENGTH = 3;
+    this.WAITING_NEW_LENGTH_END = 4;
+    this.length = 0;
+    this.content = [];
+    this.isFinish = false;
+    this.current = this.WAITING_LENGTH;
+  }
+
+  receiveChar(char) {
+    if (this.current === this.WAITING_LENGTH) {
+      if (char === "\r") {
+        if (this.length === 0) {
+          console.log(this.content);
+          this.isFinish = true;
+        }
+        this.current = this.WAITING_LENGTH_LINE_END;
+      } else {
+        this.length *= 10;
+        this.length += char.charCodeAt(0) - "0".charCodeAt(0);
+      }
+    } else if (this.current === this.WAITING_LENGTH_LINE_END) {
+      if (char === "\n") {
+        this.current = this.READING_TRUNK;
+      }
+    } else if (this.current === this.READING_TRUNK) {
+      this.content.push(char);
+      this.length--;
+      if (this.length === 0) {
+        this.current = this.WAITING_NEW_LENGTH;
+      }
+    } else if (this.current === this.WAITING_NEW_LENGTH) {
+      if (char === "\r") {
+        this.current = this.WAITING_NEW_LENGTH_END;
+      }
+    } else if (this.current === this.WAITING_NEW_LENGTH_END) {
+      if (char === "\n") {
+        this.current = this.WAITING_LENGTH;
+      }
+    }
+  }
+}
+
+void (async function () {
   let request = new Request({
     method: "POST",
     host: "127.0.0.1",
     port: "8088",
-    path: '/',
+    path: "/",
     body: {
       name: "chuchen",
     },
     headers: {
-      Foo2: 'test'
-    }
+      Foo2: "test",
+    },
   });
-  
+
   let response = await request.send();
   console.log(response);
-}();
+})();
 
 // class Response {}
 // const client = net.createConnection({ host: "127.0.0.1", port: 8088 }, () => {
