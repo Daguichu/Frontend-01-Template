cc9b97e3fcef241ee0bc7c04cf7ce7b60d08a3a5
chuchen <18725716749@163.com>
2020-06-05 17:06:44 +0800

    ups

--- week08/match.js
+++ week08/match.js
@@ -5,23 +5,32 @@ let attrName = "";
 let attrValue = "";
 let tagName = "";
 
+//只匹配了复合选择器、后代复杂选择器，有待优化
+//已经不知道写的是什么了
 function match(selector, element) {
   const selectorList = selector.split(" ");
   const lastSelector = selectorList[selectorList.length - 1];
   let parent = element.parentNode;
 
+  //首先查看最后一个选择器与当前元素是否完全匹配
+  //使用状态机将当前选择器保存到初始化的变量里
   let state = startSelector;
   for (let i of lastSelector) {
     state = state(i);
   }
   const matchElem = check(element);
+  //查看父元素是否与之前选择器匹配
   let len = selectorList.length,
     i;
   if (matchElem) {
+    //只是复合选择器情况
     if (len === 1) {
       return true;
     }
+    //从后往前遍历选择器列表，将每个选择器都与其父元素进行检查是否匹配，匹配了退出父元素循环，
+    //只要父元素不为空就一直遍历选择器列表，最后判断选择器列表是否匹配完
     for (i = len - 2; i >= 0; i--) {
+      //初始化变量
       classNames = [];
       id = "";
       attrs = {};
@@ -63,6 +72,7 @@ function match(selector, element) {
   // return true;
 }
 
+//检查一个元素与当前选择器是否匹配
 function check(element) {
   const attrKeys = Object.keys(attrs);
   if (classNames.length) {
